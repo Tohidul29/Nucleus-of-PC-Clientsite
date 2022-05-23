74b84f2a31dbac7173d00b52ecde140c615bcf52
Tohidul29 <tohidswfu@gmail.com>
2022-05-24 04:45:48 +0600

    tools loaded from db and successfully show in ui

--- src/components/Home/Home/Home.js
+++ src/components/Home/Home/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Footer from '../../Shared/Footer/Footer';
 import Banner from '../Banner/Banner';
+import Tools from '../Tools/Tools';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <Tools></Tools>
             <Footer></Footer>
         </div>
     );
---
+++ src/components/Home/Tools/Tools.js
@@ -0,0 +1,26 @@
+import { faTools } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from 'react';
+import useTools from '../../../hooks/useTools';
+import Tool from './Tool/Tool';
+
+const Tools = () => {
+    const [tools, setTools] = useTools();
+    const toolsSliceing = tools.slice(0, 6);
+
+    return (
+        <div>
+        <h1 className='text-3xl font-bold text-center text-[#03203C] mt-12 mb-4'>Tools Available on Our Company Now <FontAwesomeIcon icon={faTools}></FontAwesomeIcon></h1>
+            <div className='grid lg:grid-cols-3 sm:grid-cols-1 md:grid-cols-2 lg:mx-12 lg:my-8 sm:w-full'>
+                {
+                    toolsSliceing.map(tool => <Tool
+                        key={tool._key}
+                        tool={tool}
+                    ></Tool>)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default Tools;
