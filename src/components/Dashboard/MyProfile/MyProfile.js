d56ddd357cacfc664c507964d18fa3655dcd84db
Tohidul29 <tohidswfu@gmail.com>
2022-05-28 22:05:36 +0600

    my profile added

--- src/components/Dashboard/MyProfile/MyProfile.js
+++ src/components/Dashboard/MyProfile/MyProfile.js
@@ -1,24 +1,105 @@
-import { faMailBulk, faUser } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React from 'react';
+// import { faMailBulk, faUser } from '@fortawesome/free-solid-svg-icons';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import React from 'react';
+// import { useAuthState } from 'react-firebase-hooks/auth';
+// import auth from '../../../firebase.init';
+// import userImg from '../../../img/Person/person_img.jfif'
+
+// const MyProfile = () => {
+//     const [user] = useAuthState(auth);
+//     return (
+//         <div>
+//             <div className="card w-96 bg-base-100 shadow-xl mx-auto mt-6">
+//                 <div className="w-24 mask mask-squircle mx-auto mt-6">
+//                     <img src={userImg} alt='img' />
+//                 </div>
+//                 <div className="card-body items-center text-center">
+//                     <h2 className="card-title"><FontAwesomeIcon icon={faUser}></FontAwesomeIcon> {user.displayName}</h2>
+//                     <h2 className="card-title"><FontAwesomeIcon icon={faMailBulk}></FontAwesomeIcon>{user.email}</h2>
+//                 </div>
+//             </div>
+//         </div>
+//     );
+// };
+
+// export default MyProfile;
+
+import React, { useEffect, useRef, useState } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import auth from '../../../firebase.init';
-import userImg from '../../../img/Person/person_img.jfif'
+
 
 const MyProfile = () => {
     const [user] = useAuthState(auth);
+
+    const imageRef = useRef('');
+    const educationRef = useRef('');
+    const locationRef = useRef('');
+    const phoneRef = useRef('');
+    const linkedInRef = useRef('');
+    const handleUpdateInfo = event => {
+        event.preventDefault();
+        const info = {
+            name: user.displayName,
+            email: user.email,
+            picture: imageRef.current.value,
+            education: educationRef.current.value,
+            location: locationRef.current.value,
+            phone: phoneRef.current.value,
+            linkedIn: linkedInRef.current.value
+        };
+
+        fetch(`http://localhost:5000/user/${user.email}`, {
+            method: 'PUT',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(info)
+        })
+            .then(res => res.json())
+            .then(data => console.log(data));
+    }
+
+    // my profile 
+    const [update, setUpdate] = useState([]);
+    useEffect(() => {
+        const url = `http://localhost:5000/user/${user.email}`;
+        fetch(url)
+            .then(res => res.json())
+            .then(data => setUpdate(data))
+    }, [update]);
+
     return (
-        <div>
-            <div className="card w-96 bg-base-100 shadow-xl mx-auto mt-6">
-                <div className="w-24 mask mask-squircle mx-auto mt-6">
-                    <img src={userImg} alt='img' />
-                </div>
-                <div className="card-body items-center text-center">
-                    <h2 className="card-title"><FontAwesomeIcon icon={faUser}></FontAwesomeIcon> {user.displayName}</h2>
-                    <h2 className="card-title"><FontAwesomeIcon icon={faMailBulk}></FontAwesomeIcon>{user.email}</h2>
+        <section className='grid lg:grid-cols-2 md:grid-cols-1 sm:grid-cols-1'>
+            <div class="card w-96 bg-base-100 shadow-xl ml-1">
+                <div class="card-body">
+                    <div className="avatar">
+                        <div className="w-48 mx-auto rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+                            <img src={update[0]?.picture} />
+                        </div>
+                    </div>
+                    <p><span className='font-bold'>Name:</span> {user.displayName}</p>
+                    <p><span className='font-bold'>Email:</span> {user.email}</p>
+                    <p><span className='font-bold'>Phone: </span> {update[0]?.phone}</p>
+                    <p><span className='font-bold'>Education:</span> {update[0]?.education}</p>
+                    <p><span className='font-bold'>Location:</span> {update[0]?.location}</p>
+                    <p><span className='font-bold'>LinkedIn Profile Link:</span> {update[0]?.linkedIn}</p>
+
                 </div>
             </div>
-        </div>
+
+            <div>
+                <form className='flex flex-col items-center bg-gray-100 my-8 p-8' onSubmit={handleUpdateInfo}>
+                    <h2 className='text-5xl font-bold mb-5'>Update Profile</h2>
+                    <input ref={imageRef} type="text" placeholder="Image URL" className="input w-full max-w-lg mb-3 mt-3" required />
+                    <input ref={educationRef} type="text" placeholder="Education" className="input w-full max-w-lg mb-3" required />
+                    <input ref={locationRef} type="text" placeholder="Location" className="input w-full max-w-lg mb-3" required />
+                    <input ref={phoneRef} type="number" placeholder="Phone Number" className="input w-full max-w-lg mb-3" required />
+                    <input ref={linkedInRef} type="text" placeholder="LinkedIn profile link" className="input w-full max-w-lg mb-3" required />
+                    <input className='btn btn-success w-2/4 mt-4' type="submit" value="Update Profile" />
+                </form>
+            </div>
+        </section>
     );
 };
 
