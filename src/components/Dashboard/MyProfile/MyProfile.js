25d2f25d03b0c0e3bb4b09d85b14b9187249233d
Tohidul29 <tohidswfu@gmail.com>
2022-05-29 04:53:30 +0600

    fetch url issue solved

--- src/components/Dashboard/MyProfile/MyProfile.js
+++ src/components/Dashboard/MyProfile/MyProfile.js
@@ -23,7 +23,7 @@ const MyProfile = () => {
             linkedIn: linkedInRef.current.value
         };
 
-        fetch(`http://localhost:5000/users/${user.email}`, {
+        fetch(`https://enigmatic-sea-26065.herokuapp.com/users/${user.email}`, {
             method: 'PUT',
             headers: {
                 'content-type': 'application/json'
